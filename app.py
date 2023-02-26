c6abfd58f37955bc5ac44ba7974a53db05f6f29e
Josh Burdett <jburdett1993@gmail.com>
2023-02-25 21:01:24 -0600

    adding back model code

--- app.py
+++ app.py
@@ -4,26 +4,26 @@ import pandas as pd
 import joblib
 
 
-# model_name = "Titanic survivability machine learning model"
-# model_file = 'model.pkl'
-# version = "v1.0.0"
+model_name = "Titanic survivability machine learning model"
+model_file = 'model.pkl'
+version = "v1.0.0"
 
-# clf = joblib.load('model/model.pkl')
+clf = joblib.load('model/model.pkl')
 
-# # helper method to receive the inference request as JSON, convert to df, then predict
-# # the method then assigns the label variable to a more descriptive response
-# def get_model_response(json_data):
-#     X = pd.DataFrame.from_dict(json_data)
-#     prediction = clf.predict(X)[0]
-#     if prediction == 1:
-#         label = "A person with these stats would have survived"
-#     else:
-#         label = "A person with these stats would have died"
-#     return {
-#         'status': 200,
-#         'label': label,
-#         'prediction': int(prediction)
-#     }
+# helper method to receive the inference request as JSON, convert to df, then predict
+# the method then assigns the label variable to a more descriptive response
+def get_model_response(json_data):
+    X = pd.DataFrame.from_dict(json_data)
+    prediction = clf.predict(X)[0]
+    if prediction == 1:
+        label = "A person with these stats would have survived"
+    else:
+        label = "A person with these stats would have died"
+    return {
+        'status': 200,
+        'label': label,
+        'prediction': int(prediction)
+    }
 
 
 # instantiate a Flask app
@@ -34,39 +34,39 @@ def index():
     return "<h1>Titanic-Flask server active</h1>"
 
 
-# @app.route('/info', methods=['GET'])
-# def info():
-#     """Return model information, version, how to call"""
-#     result = {}
+@app.route('/info', methods=['GET'])
+def info():
+    """Return model information, version, how to call"""
+    result = {}
 
-#     result["name"] = model_name
-#     result["version"] = version
+    result["name"] = model_name
+    result["version"] = version
 
-#     return result
-
-
-# @app.route('/health', methods=['GET'])
-# def health():
-#     """Return service health"""
-#     return 'ok'
+    return result
 
 
-# @app.route('/predict', methods=['POST'])
-# def predict():
-#     feature_dict = request.get_json()
-#     if not feature_dict:
-#         return {
-#             'error': 'Body is empty.'
-#         }, 500
+@app.route('/health', methods=['GET'])
+def health():
+    """Return service health"""
+    return 'ok'
 
-#     try:
-#         response = get_model_response(feature_dict)
-#     except ValueError as e:
-#         return {'error': str(e).split('\n')[-1].strip()}, 500
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    feature_dict = request.get_json()
+    if not feature_dict:
+        return {
+            'error': 'Body is empty.'
+        }, 500
+
+    try:
+        response = get_model_response(feature_dict)
+    except ValueError as e:
+        return {'error': str(e).split('\n')[-1].strip()}, 500
     
-#     # TODO set up validations for features the model expects, as well as feature types 
+    # TODO set up validations for features the model expects, as well as feature types 
 
-#     return response, 200
+    return response, 200
 
 # assign the `clf` variable to the local model file that was created during training
 if __name__ == '__main__':
